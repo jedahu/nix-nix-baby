a2966720871e092842259fe10cceb972e9048aab
Jeremy Hughes <jedahu@gmail.com>
2022-03-04 12:03:42 +1300

    comment less term config

--- home/env.nix
+++ home/env.nix
@@ -12,13 +12,13 @@ lib.mkMerge [{
     SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
     GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
-    LESS_TERMCAP_mb="$'\e[1;32m'";
-    LESS_TERMCAP_md="$'\e[1;32m'";
-    LESS_TERMCAP_me="$'\e[0m'";
-    LESS_TERMCAP_se="$'\e[0m'";
-    LESS_TERMCAP_so="$'\e[01;33m'";
-    LESS_TERMCAP_ue="$'\e[0m'";
-    LESS_TERMCAP_us="$'\e[1;4;31m'";
+    # LESS_TERMCAP_mb="$'\e[1;32m'";
+    # LESS_TERMCAP_md="$'\e[1;32m'";
+    # LESS_TERMCAP_me="$'\e[0m'";
+    # LESS_TERMCAP_se="$'\e[0m'";
+    # LESS_TERMCAP_so="$'\e[01;33m'";
+    # LESS_TERMCAP_ue="$'\e[0m'";
+    # LESS_TERMCAP_us="$'\e[1;4;31m'";
 
     GPG_TTY = "$(tty)";
     SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
